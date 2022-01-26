89727aa7cd8d5b438a135caabc6f31a0646b6097
Martha Nieto <m27842n@outlook.com>
2022-01-25 21:58:37 -0800

    - update:main - functionality to tabs showing buttons pagination.

--- src/js/app.js
+++ src/js/app.js
@@ -5,7 +5,9 @@ document.addEventListener('DOMContentLoaded', function() {
 });
 
 function iniciarApp() {
+    mostrarSeccion();
     tabs(); //Cambia la seccion cuando se presionen los tabs
+    botonesPaginador();
 }
 
 function mostrarSeccion() {
@@ -17,6 +19,14 @@ function mostrarSeccion() {
     const pasoSelector = `#paso-${paso}`;
     const seccion = document.querySelector(pasoSelector);
     seccion.classList.add('mostrar');
+
+    const tabAnterior = document.querySelector('.actual');
+    if(tabAnterior) {
+        tabAnterior.classList.remove('actual');
+    }
+
+    const tab = document.querySelector(`[data-paso="${paso}"]`);
+    tab.classList.add('actual');
 }
 
 function tabs() {
@@ -27,6 +37,23 @@ function tabs() {
             paso = parseInt(e.target.dataset.paso);
 
             mostrarSeccion();
+            botonesPaginador();
         });
     })
+}
+
+function botonesPaginador() {
+    const paginaSiguiente = document.querySelector('#siguiente');
+    const paginaAnterior = document.querySelector('#anterior');
+    
+    if(paso === 1) {
+        paginaAnterior.classList.add('ocultar');
+        paginaSiguiente.classList.remove('ocultar');
+    } else if(paso === 3 ) {
+        paginaAnterior.classList.remove('ocultar');
+        paginaSiguiente.classList.add('ocultar');
+    } else {
+        paginaAnterior.classList.remove('ocultar');
+        paginaSiguiente.classList.remove('ocultar');
+    }
 }
