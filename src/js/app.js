3bd18c67bafd5e0929d08e5bf0cef4e08eb93a9e
Martha Nieto <m27842n@outlook.com>
2022-02-03 18:24:47 -0800

    - update:main - added fetch request to services.

--- src/js/app.js
+++ src/js/app.js
@@ -310,8 +310,15 @@ function mostrarResumen()
 
 async function reservarCita()
 {
+    const { nombre, fecha, hora, servicios } = cita;
+
+    const idServicios = servicios.map(servicio => servicio.id);
+
     const datos = new FormData();
-    datos.append('nombre', 'Martha');
+    datos.append('nombre', nombre);
+    datos.append('fecha', fecha);
+    datos.append('hora', hora);
+    datos.append('servicios', idServicios);
 
     const url = 'http://localhost:3000/api/appointments';
 
