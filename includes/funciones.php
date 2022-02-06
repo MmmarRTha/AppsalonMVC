05023a659f05449f1168cf48c9108b3355ba0fb5
Martha Nieto <m27842n@outlook.com>
2022-02-05 18:36:33 -0800

    - update:main - added authentication to appointments route.

--- includes/funciones.php
+++ includes/funciones.php
@@ -12,3 +12,12 @@ function s($html) : string {
     $s = htmlspecialchars($html);
     return $s;
 }
+
+//funcion que revisa que el usuario este autenticado
+function isAuth() : void
+{
+    if(!isset($_SESSION['login']))
+    {
+        header('Location: /');
+    }
+}
