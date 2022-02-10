45e066a25e12b71825fd88833e7d88deb021289e
Martha Nieto <m27842n@outlook.com>
2022-02-09 20:47:29 -0800

    - update:main - added css to services.

--- views/servicios/crear.php
+++ views/servicios/crear.php
@@ -1,6 +1,7 @@
 <h1 class="nombre-pagina">Nuevo Servicio</h1>
 <p class="descripcion-pagina">Llena todos los campos para anadir un nuevo servicio</p>
 
+<?php include_once __DIR__ . '/../templates/barra.php'; ?>
 <?php include_once __DIR__ . '/../templates/alertas.php'; ?>
 
 <form action="/servicios/crear" method="POST" class="formulario">
